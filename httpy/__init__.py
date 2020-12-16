bc2db1a4a3546f904e2bc6228f82b3d2f2e91ced
Mahtat <yassinmhtat@gmail.com>
2020-12-16 22:22:41 +0100

    Add the `__init__.py` file to the project

---
+++ httpy/__init__.py
@@ -0,0 +1,19 @@
+"""HyperText Transfer Protocol, Python module
+"""
+
+# Author info
+__email__ = "yassinmhtat@gmail.com"
+__author__ = "MAHTAT Yassin"
+
+# Project info
+__title__ = "HyperText Transfer Protocol, Python implementation"
+__url__ = "https://github.com/mahtat555/HttPy.git"
+__license__ = "MIT"
+__date__ = "2020-12-11"
+__version__ = "0.0.1"
+
+# List of public objects in this package
+__all__ = [
+    "__email__", "__author__", "__title__", "__url__", "__license__",
+    "__date__", "__version__"
+]
