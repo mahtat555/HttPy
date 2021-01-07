d6a2da3692015ad37398613aeb21d7c32490c748
Mahtat <yassinmhtat@gmail.com>
2021-01-07 18:46:48 +0100

    Add `adds` method to the `_Data` class

--- httpy/utils.py
+++ httpy/utils.py
@@ -19,6 +19,13 @@ class _Data:
         """
         self.__dict__[key] = value
 
+    def adds(self, **kwargs):
+        """Add attributes to the object.
+
+        """
+        for key, value in kwargs.items():
+            self.__dict__[key] = value
+
     def __str__(self):
         return type(self).__name__ + "({})".format(", ".join(
             ["{}='{}'".format(k, v) for k, v in self.__dict__.items()]
