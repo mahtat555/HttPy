7388dc7ac4c3c808860d21b86e418cbcfabbf7ba
Mahtat <yassinmhtat@gmail.com>
2021-01-08 21:08:49 +0100

    Add the `URL` class to the project

---
+++ httpy/urls.py
@@ -0,0 +1,12 @@
+"""In this module, we define classes that implement the URLs.
+
+"""
+
+from .utils import Data
+
+
+class URL(Data):
+    """ URL class
+    This class represents the different elements of a URL.
+
+    """
