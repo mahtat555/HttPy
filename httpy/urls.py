dd4bbc12fe580850e28cf0a011da795b69ff46bd
Mahtat <yassinmhtat@gmail.com>
2021-01-21 15:54:42 +0100

    Add the `pathsplit()` function to the project

--- httpy/urls.py
+++ httpy/urls.py
@@ -2,9 +2,29 @@
 
 """
 
+from collections import namedtuple
+
+# Structured result objects
+Path = namedtuple("Path", ("path", "query", "signet"))
+
 
 class URL:
     """ URL class
     This class represents the different elements of a URL.
 
     """
+
+
+def pathsplit(path):
+    """ Parse a Path into its components.
+
+    """
+    signet = ""
+    if "#" in path:
+        path, signet = path.split('#', 1)
+
+    query = ""
+    if "?" in path:
+        path, query = path.split('?', 1)
+
+    return Path(path=path, query=query, signet=signet)
