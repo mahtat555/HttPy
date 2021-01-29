ba2f5f2c186659f655f0c9011f4efb7547fee567
Mahtat <yassinmhtat@gmail.com>
2021-01-29 22:26:30 +0100

    Add `urlencode()` function to the project

--- httpy/urls.py
+++ httpy/urls.py
@@ -4,6 +4,23 @@
 
 from collections import namedtuple
 
+
+# These characters are always safe (not change) during the encoding process.
+ALWAYS_SAFE = (
+    b'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    b'abcdefghijklmnopqrstuvwxyz'
+    b'0123456789'
+    b'_.-'
+)
+
+
+HEX_DIGITS = "0123456789abcdefABCDEF"
+
+HEX_BYTE = dict(
+    ((x + y).encode(), bytes.fromhex(x + y))
+    for x in HEX_DIGITS for y in HEX_DIGITS
+)
+
 # Structured result objects
 Path = namedtuple("Path", ("path", "query", "signet"))
 
@@ -79,3 +96,43 @@ def dict2query(_dict):
         query.append("{}={}".format(key, value))
 
     return "&".join(query)
+
+
+def urlencode(data, safe=b"", plus=False):
+    """ The purpose of this function is to encode a URL.
+
+    """
+    # for encoding the content of an HTTP request
+    if plus:
+        safe += b" "
+    # for encoding URLs
+    else:
+        safe += b"!#$&'()*+,/:;=?@~?"
+
+    if isinstance(data, str):
+        data = data.encode()
+
+    data = data.split(b"%")
+    encode = _urlencode(data[0], safe)
+
+    for _data in data[1:]:
+        if _data[:2] in HEX_BYTE:
+            encode += "%" + _data[:2].decode() + _urlencode(_data[2:], safe)
+        else:
+            encode += _urlencode(b"%" + _data, safe)
+
+    return encode
+
+
+def _urlencode(data, safe=b""):
+
+    safe += ALWAYS_SAFE
+    _data = ""
+
+    for byte in data:
+        if byte in safe:
+            _data += chr(byte)
+        else:
+            _data += "%{0:02X}".format(byte)
+
+    return _data.replace(" ", "+")
